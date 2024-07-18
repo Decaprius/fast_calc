3caa30cb9ad453fdaaef3573c1aa35f0c532a73c
Michael Fössinger <2210653977stud.fh-kufstein.ac.at>
2024-07-18 13:42:18 +0200

    Implemented main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'util.dart';
 import 'theme.dart';
 
 void main() {
@@ -8,11 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ThemeData lightTheme = const MaterialTheme(TextTheme()).light();
-    ThemeData darkTheme = const MaterialTheme(TextTheme()).dark();
+    TextTheme textTheme = createTextTheme(context, 'Roboto', 'Roboto');
+    ThemeData lightTheme = MaterialTheme(textTheme).light();
+    ThemeData darkTheme = MaterialTheme(textTheme).dark();
 
     return MaterialApp(
       title: 'FastCalc',
@@ -40,11 +42,73 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                      child: _buildNavButton(Icons.cottage, "Heizlast", () {})),
+                  const SizedBox(width: 16),
+                  Expanded(
+                      child: _buildNavButton(
+                          Icons.vertical_shades_closed, "FBH", () {})),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(child: _buildNavButton(Icons.air, "Abluft", () {})),
+                  const SizedBox(width: 16),
+                  Expanded(
+                      child: _buildNavButton(Icons.shower, "Abwasser", () {})),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                      child: _buildNavButton(
+                          Icons.local_fire_department, "Heizung", () {})),
+                  const SizedBox(width: 16),
+                  Expanded(
+                      child:
+                          _buildNavButton(Icons.water_drop, "Sanitär", () {})),
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildNavButton(IconData icon, String label, VoidCallback onPressed) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 48),
+          const SizedBox(height: 8),
+          Text(label, style: Theme.of(context).textTheme.titleMedium),
+        ],
       ),
     );
   }
